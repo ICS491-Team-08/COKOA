817064da1999fcaca5e3e4635804c677a78b2b7c
Yeji Han <hany7@hawaii.edu>
2021-10-14 21:07:29 -1000

    Update Signin.jsx

--- app/imports/ui/pages/Signin.jsx
+++ app/imports/ui/pages/Signin.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link, Redirect } from 'react-router-dom';
 import { Meteor } from 'meteor/meteor';
-import { Container, Form, Grid, Header, Message, Segment } from 'semantic-ui-react';
+import { Container, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react';
 
 /**
  * Signin page overrides the form’s submit event and call Meteor’s loginWithPassword().
@@ -50,6 +50,10 @@ export default class Signin extends React.Component {
             </Header>
             <Form onSubmit={this.submit}>
               <Segment stacked>
+                <b className="logo-name">
+                  <Image
+                      src="/images/cokoa.png"/>
+                </b>
                 <Form.Input
                   label="Email"
                   id="signin-form-email"
